3d40dbda598cbebefd3503367cde0223c3fd2805
Dominik Pieczyński <dominik.pieczynski@gmail.com>
2015-04-06 03:13:50 +0200

    Make Leds work with inherited GPIOPins class

--- include/Leds.hpp
+++ include/Leds.hpp
@@ -1,41 +1,25 @@
 #ifndef LEDS_HPP
 #define LEDS_HPP
 
-#include <bitset>
+#include "GPIO.hpp"
 
 #include "stm32f4xx.h"
-#include "stm32f4xx_hal_gpio.h"
-#include "stm32f4xx_hal_rcc.h"
+
 
 /**
- * Contains convenient names for LEDs.
+ * Convenient names for LEDs
  */
-enum class Led : uint16_t {
-	Green = GPIO_PIN_12,
-	Orange = GPIO_PIN_13,
-	Red = GPIO_PIN_14,
-	Blue = GPIO_PIN_15
-};
+#define GreenLed GPIO::Pin::P12
+#define OrangeLed GPIO::Pin::P13
+#define RedLed GPIO::Pin::P14
+#define BlueLed GPIO::Pin::P15
 
 /**
  * Implements basic operations with on-board leds.
  * Requires the __init_.hpp header to be included after this class inclusion.
  */
-class Leds {
-    private:
-        static void initialize();
-		Leds();
-
+class Leds : public GPIO::GPIOPins {
     public:
-        static bool __initialized;
-        
-		/**
-		 * @brief Turns all given Leds on.
-		 */
-		static void turn_on(std::initializer_list<Led> leds);
-		/**
-		 * @brief Turns all given Leds off.
-		 */
-		static void turn_off(std::initializer_list<Led> leds);
+		Leds(std::initializer_list<GPIO::Pin> leds);
 };
 #endif // LEDS_HPP
