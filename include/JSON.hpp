0f8ccfc4a512eb4c5f8c743ffb9e8fff7adae48f
Dominik Pieczyński <dominik.pieczynski@gmail.com>
2015-06-17 03:02:32 +0200

    Add support for basic JSON serialization

--- include/JSON.hpp
+++ include/JSON.hpp
@@ -7,10 +7,19 @@
 #include <experimental/any>
 #include "debug.h"
 
+struct Range {
+    int begin;
+    int end;
+};
+
 class JSON {
     private:
         std::unordered_map <std::string, std::experimental::any> deserialized;
+        std::vector <std::string> inserted_indexes;
+        std::vector <std::experimental::any> inserted_values;
+        std::vector <Range> inserted_objects;
         std::experimental::any deserialize_value(std::string value);
+        std::string get_representation(std::experimental::any value);
 
         /* This is probably slow as hell */
         template <typename T>
@@ -25,8 +34,19 @@ class JSON {
     public:
         JSON(std::string json_string);
         JSON() {};
+        std::string serialize();
         template <typename T>
         T get(std::string index) {return std::experimental::any_cast<T>(deserialized[index]);};
+        template <typename T>
+        void add(std::string index, T value) {
+            if (inserted_objects.size() == 0) {
+                add_object();
+            }
+            inserted_indexes.push_back(index);
+            inserted_values.emplace_back(std::experimental::any(value));
+            inserted_objects.back().end++;
+        }
+        void add_object();
         bool is_empty() const {return deserialized.size() == 0;}
 };
 
--- src/JSON.cpp
+++ src/JSON.cpp
@@ -30,3 +30,82 @@ std::experimental::any JSON::deserialize_value(std::string value) {
         return std::experimental::any(lexical_cast<int>(value));
     }
 }
+
+std::string JSON::get_representation(std::experimental::any value) {
+    std::string value_type_name = value.type().name();
+
+    if (value_type_name == "i") {
+        std::stringstream result;
+        result << std::experimental::any_cast<int>(value);
+        return std::string(result.str());
+    }
+    else if (value_type_name == "Ss") {
+        std::string result = "\"";
+        result += std::experimental::any_cast<std::string>(value);
+        result += "\"";
+        return result;
+    }
+    else if (value_type_name == "PKc") {
+        std::string result = "\"";
+        result += std::experimental::any_cast<const char *>(value);
+        result += "\"";
+        return result;
+    }
+    else if (value_type_name == "d") {
+        std::stringstream result;
+        result << std::experimental::any_cast<double>(value);
+        return std::string(result.str());
+    }
+    else if (value_type_name == "f") {
+        std::stringstream result;
+        result << std::experimental::any_cast<float>(value);
+        return std::string(result.str());
+    }
+    else {
+        return "";
+    }
+}
+
+std::string JSON::serialize() {
+    unsigned long number_of_objects = inserted_objects.size();
+
+    std::string serialized;
+    if (number_of_objects > 1) {
+        serialized += "[";
+    }
+
+    for (int i = 0; i < number_of_objects; i++) {
+        serialized += "{\n";
+
+        for (int j = inserted_objects[i].begin; j <= inserted_objects[i].end; j++) {
+            serialized += "\"";
+            serialized += inserted_indexes[j];
+            serialized += "\": ";
+            serialized += get_representation(inserted_values[j]);
+
+            if (inserted_objects[i].end - j != 0) {
+                serialized += ",";
+            }
+
+            serialized += "\n";
+        }
+
+        if (number_of_objects - i == 1) {
+            serialized += "}";
+        }
+        else {
+            serialized += "},\n";
+        }
+    }
+
+    if (number_of_objects > 1) {
+        serialized += "]";
+    }
+
+    return serialized;
+}
+
+void JSON::add_object() {
+    unsigned int start_index = inserted_values.size();
+    inserted_objects.emplace_back(Range{start_index, start_index - 1});
+}
