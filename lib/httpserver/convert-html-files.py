79e9b6e97674be5b968704939d76504909a87017
Reikion <Reikion@users.noreply.github.com>
2015-06-17 15:56:07 +0200

    Update convert-html-files script to generate file unordered map

--- lib/httpserver/convert-html-files.py
+++ lib/httpserver/convert-html-files.py
@@ -5,6 +5,19 @@ import re
 import os
 from datetime import datetime
 
+class Unordered_map_gen:
+    __filemap = []
+    @staticmethod
+    def add(html_filepath :str):
+        # example file_hash_table["/"]  = &htmlfile__index_html;
+
+        append_string = "file_hash_table[\"{html_filepath}\"] = &{filesymbol};".format(html_filepath = html_filepath,
+                                                                                       filesymbol = "htmlfile_" + convert_str_to_file_symbol(html_filepath))
+        Unordered_map_gen.__filemap.append(append_string)
+
+    @staticmethod
+    def get():
+        return Unordered_map_gen.__filemap
 
 def get_last_modified_str(path: str) -> str:
     stat_time = os.stat(path).st_mtime
@@ -56,28 +69,54 @@ def generate_file(input_path: str, output_path: str, html_filepath: str):
         print("};\n", file=output_file)
 
 
+
 def main():
     # args parsing
     parser = argparse.ArgumentParser(description='Generate resource file from html files from given folder. ')
     parser.add_argument("folder", help="folder, where source files are located")
-    parser.add_argument("output_file", help="placement of generated resources' file")
+    parser.add_argument("output_file_cpp", help="placement of generated resources' file")
+    parser.add_argument("output_file_hpp", help="placement of header of resources' file")
     parser.add_argument("-f", dest="file_overwrite", action="store_true", default=False, help="overwrite file if exist")
     args = parser.parse_args()
 
     # args correctness checking
     if not os.path.isdir(args.folder):
         sys.exit("Error: given folder path isn't folder")
-    elif os.path.isdir(args.output_file):
+    elif os.path.isdir(args.output_file_cpp) or os.path.isdir(args.output_file_hpp) :
         sys.exit("Error: output file should be file, not a folder.")
-    elif os.path.isfile(args.output_file) and not args.file_overwrite:
+    elif (os.path.isfile(args.output_file_cpp) or os.path.isfile(args.output_file_hpp)) and not args.file_overwrite:
         sys.exit("Error: output file exists. Use '-f' to overwrite.")
-    elif (not os.path.exists(args.output_file)) or (os.path.isfile(args.output_file) and args.file_overwrite):
+    elif ( ((not os.path.exists(args.output_file_cpp)) and (not os.path.exists(args.output_file_hpp))) or 
+         (os.path.isfile(args.output_file_cpp) and os.path.isfile(args.output_file_hpp) and args.file_overwrite) ):
         # main program
 
-        wipe_file(args.output_file)
+        wipe_file(args.output_file_cpp)
+        wipe_file(args.output_file_hpp)
+
+        # make header
+        with open(args.output_file_hpp, "a") as header_file:
+            print(
+                "#include <unordered_map>\n"
+                "\n"
+                "#include \"html_file.hpp\"\n"
+                "\n"
+                "std::unordered_map<std::string, Html_file*> populate_file_umap();\n",
+                file=header_file
+            ) 
+
+        # add header to cpp file
+        with open(args.output_file_cpp, "a") as cpp_file:
+            print(
+                "#include \"{header_file_str}\"\n"
+                "\n".format(header_file_str=os.path.basename(args.output_file_hpp)),
+                file=cpp_file
+            ) 
+
+        # generate and save resources to cpp file
         dir_list = args.folder.split("/")
         for root, dirs, files in os.walk(args.folder):
             for file in files:
+                print("Processing file " + root + "/" + file + "...")
                 root_list = root.split("/")
 
                 # strip folder path from html folder path
@@ -89,7 +128,22 @@ def main():
                 else:
                     html_filepath = "/" + file
 
-                generate_file(root + "/" + file, args.output_file, html_filepath)
+                generate_file(root + "/" + file, args.output_file_cpp, html_filepath)
+                Unordered_map_gen.add(html_filepath)
+
+        # generate function to populate unordered map
+        with open(args.output_file_cpp, "a") as cpp_file:
+            print(
+                "std::unordered_map<std::string, Html_file*> populate_file_umap()\n"
+                "{{\n"
+                "    std::unordered_map<std::string, Html_file*> file_hash_table;\n"
+                "    {file_unordered_map}\n"
+                "    return file_hash_table;\n"
+                "}}\n".format(file_unordered_map = "\n    ".join(Unordered_map_gen.get())),
+                file=cpp_file
+            ) 
+
+
 
 
 if __name__ == "__main__":
